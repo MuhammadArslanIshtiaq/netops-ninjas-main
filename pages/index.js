85ad65238a654e75d0b57ae536ec16e0a57d45a9
MuhammadArslanIshtiaq <muhammadarslanishtiaq@gmail.com>
2023-02-23 18:18:35 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import AboutSection from "../components/sections/AboutSection";
 import CtaSection from "../components/sections/CtaSection";
 import HeroSection from "../components/sections/HeroSection";
-import OurClientsSection from "../components/sections/OurClientsSection";
 import TestimonialSection from "../components/sections/TestimonialSection";
 import WhatWeDoSection from "../components/sections/WhatWeDoSection";
 import WhyNetopsSection from "../components/sections/WhyNetopsSection";
@@ -18,7 +17,6 @@ export default function Home() {
         <WhatWeDoSection />
         <AboutSection />
         <WhyNetopsSection />
-        <OurClientsSection />
         <TestimonialSection />
         <CtaSection />
       </main>
