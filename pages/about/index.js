f3536f15b6963cb6945b439ce7389a0ddf0f4ebf
HassanNetops <133920441+HassanNetops@users.noreply.github.com>
2023-08-27 23:23:22 +0100

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -16,7 +16,6 @@ const supportLinks = [
   {
     number: "20K",
     title: "Problems Troubleshoot",
-    
   },
 ];
 
@@ -88,48 +87,19 @@ const About = () => {
         <section className="bg-section text-white pt-12 sm:pt-20">
           <Container>
             <div className="my-20 max-w-4xl mx-auto text-center pt-2">
-             <h5 className="inline-block text-lg font-extrabold uppercase text-gradient mb-3">
-                About us
-              </h5> 
               <h2 className="text-3xl sm:text-4xl lg:text-[2.75rem] font-bold leading-tight sm:leading-tight xl:leading-tight">
                 Unlock your true business&apos;s potential with our 360-degree IT Solutions - Partner with us today!
               </h2>
               <p className="mt-6 max-w-xl mx-auto text-[1.375rem] text-gray-200">
                 From IT infrastructure to cybersecurity, digital marketing to 
                 cloud solutions, our expertise covers every aspect of your technology 
-                needs.With a customer-centric approach and a team of experienced professionals, 
+                needs. With a customer-centric approach and a team of experienced professionals, 
                 we deliver tailored solutions to help businesses thrive in the digital era.
               </p>
             </div>
           </Container>
         </section>
-        <section className="bg-section text-white pt-12 sm:pt-20">
-          <div className="mx-auto max-w-xl px-6 lg:max-w-7xl lg:px-8">
-            <dl className="grid grid-cols-1 gap-16 lg:grid lg:grid-cols-3">
-              {features.map((feature) => (
-                <div key={feature.name}>
-                  <dt>
-                    <div className="flex justify-center">
-                      <Image
-                        className="object-contain"
-                        width={85}
-                        height={85}
-                        src={feature.iconUrl}
-                        alt=""
-                      />
-                    </div>
-                    <p className="mt-6 text-lg font-semibold leading-8 tracking-tight text-center">
-                      {feature.name}
-                    </p>
-                  </dt>
-                  <dd className="mt-2 text-base leading-7 text-center text-84">
-                    {feature.description}
-                  </dd>
-                </div>
-              ))}
-            </dl>
-          </div>
-        </section>
+
         <CtaSection />
       </main>
     </>
