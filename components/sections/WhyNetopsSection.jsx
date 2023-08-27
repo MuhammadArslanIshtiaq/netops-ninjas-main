dd2115ad3a7b377098188b9503351df3d917d512
HassanNetops <133920441+HassanNetops@users.noreply.github.com>
2023-08-27 22:17:29 +0100

    Update WhyNetopsSection.jsx

--- components/sections/WhyNetopsSection.jsx
+++ components/sections/WhyNetopsSection.jsx
@@ -26,7 +26,7 @@ const netopCards = [
 
 const brandList = [
   { logoUrl: "/assets/images/Avallation.png" },
-  { logoUrl: "/assets/images/TEC Solution.png" },
+  { logoUrl: "/assets/images/Tec-S.png" },
   { logoUrl: "/assets/images/b-logo-2.png" },
   { logoUrl: "/assets/images/b-logo-3.png" },
   { logoUrl: "/assets/images/b-logo-4.png" },
