57be5ea9bd9051812843c012c80b7ef3ffa8c5ab
HassanNetops <133920441+HassanNetops@users.noreply.github.com>
2023-08-28 01:33:31 +0100

    Update index.js

--- pages/infrastructure/index.js
+++ pages/infrastructure/index.js
@@ -11,15 +11,15 @@ import TestimonialSection from "../../components/sections/TestimonialSection";
 const serviceList = [
   {
     title: "Windows",
-    description: `Lorem ipsum dolor sit amet consiscutor moriko namas mukino. Lorem ipsum dolor sit amet consiscutor moriko namas mukino.`,
+    description: `Embrace the possibilities with Windows – unleash productivity and innovation with a trusted operating system.`,
   },
   {
     title: "Linux",
-    description: `Lorem ipsum dolor sit amet consiscutor moriko namas mukino. Lorem ipsum dolor sit amet consiscutor moriko namas mukino.`,
+    description: `Experience the freedom and flexibility of Linux – harness the power of open-source technology for your business.`,
   },
   {
     title: "SQL Server",
-    description: `Lorem ipsum dolor sit amet consiscutor moriko namas mukino. Lorem ipsum dolor sit amet consiscutor moriko namas mukino.`,
+    description: `Optimise data management and drive insights with SQL Server – harness the potential of your data.`,
   },
 ];
 
@@ -36,13 +36,13 @@ const NetworkingPage = () => {
               <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
                 <div className="mb-24 lg:mb-0 text-center lg:text-left">
                   <h2 className="text-3xl sm:text-4xl lg:text-[2.75rem] font-semibold leading-tight sm:leading-tight xl:leading-tight mb-6">
-                    We do the IT: you <br />
-                    grow your Business
+                    Solid Foundation, Limitless Possibilities: Transform <br />
+                    Your Business with our Cutting-Edge Infrastructure Solutions
                   </h2>
                   <p className="mb-16 lg:max-w-xl text-[1.375rem] text-gray-200">
-                    Lorem ipsum dolor sit amet consectetur. Justo in rho orci
-                    velit placerat enim. Convallis vitae enim vitae mauris
-                    cursus vitae urna amet.
+                    Partner with us to revolutionize your infrastructure, 
+                    ensuring robust performance, security, and efficiency 
+                    for your business needs.
                   </p>
                   <Link
                     href="/contact"
@@ -73,12 +73,12 @@ const NetworkingPage = () => {
           <Container>
             <div className="mb-12 max-w-4xl mx-auto text-center pt-2">
               <h2 className="text-3xl sm:text-4xl lg:text-[2.75rem] font-semibold leading-tight sm:leading-tight xl:leading-tight">
-                Lorem Ipsum Dolor Sit Amet Consisc
+                Building the Foundation for Success: Transforming Businesses with Robust Infrastructure Solutions
               </h2>
               <p className="mt-6 text-[1.375rem] text-gray-400">
-                Lorem ipsum dolor sit amet consectetur. Justo in rho orci velit
-                placerat enim. Convallis vitae enim vitae mauris cursus vitae
-                urna amet.
+                From robust hardware to resilient software, we design and implement tailored 
+                infrastructure solutions that lay the foundation for your business's success. 
+                Experience enhanced performance, security, and reliability
               </p>
             </div>
             <div className="py-12 sm:py-20">
